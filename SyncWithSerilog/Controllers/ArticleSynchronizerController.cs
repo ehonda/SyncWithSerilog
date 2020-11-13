d7dc482b550df9cff0589c29548a33e8a4a12acb
Dennis Renz <dennis@r-renz.de>
2020-11-13 22:10:08 +0100

    Add sln and controller

---
+++ SyncWithSerilog/Controllers/ArticleSynchronizerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SyncWithSerilog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleSynchronizerController : ControllerBase
+    {
+
+        // POST api/<ArticleSynchronizerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
