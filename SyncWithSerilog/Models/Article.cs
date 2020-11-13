76ea77153de8289c6c839854b70a5c198b4c49ca
Dennis Renz <dennis@r-renz.de>
2020-11-13 22:55:14 +0100

    Add article model, implement mock sync

---
+++ SyncWithSerilog/Models/Article.cs
@@ -0,0 +1,7 @@
+﻿namespace SyncWithSerilog.Models
+{
+    public class Article
+    {
+        public string Sku { get; set; }
+    }
+}
--- SyncWithSerilog/Synchronizer/ArticleSynchronizer.cs
+++ SyncWithSerilog/Synchronizer/ArticleSynchronizer.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using SyncWithSerilog.Models;
+using System;
+using System.Collections.Generic;
 
 namespace SyncWithSerilog.Synchronizer
 {
     public class ArticleSynchronizer
     {
-        public void Run() => RandomAction()();
+        private readonly Random _rng = new Random();
 
-        private Action RandomAction()
-            => new Random().Next(4) switch
+        public void Run()
+        {
+            var articles = GetArticles();
+            UploadArticles(articles);
+        }
+
+        private IEnumerable<Article> GetArticles()
+            => new[]
             {
-                0 => () => Console.WriteLine("Successful upload"),
-                1 => () => Console.WriteLine("Conversion error"),
-                _ => () => Console.WriteLine("Something happened")
+                new Article { Sku = "100" },
+                new Article { Sku = "101" },
+                new Article { Sku = "102" },
+                new Article { Sku = "103" }
             };
+
+        private void UploadArticles(IEnumerable<Article> articles)
+        {
+            foreach (var article in articles)
+                if (UploadArticle(article))
+                    Console.WriteLine($"Upload of article {article.Sku} succeeded");
+                else
+                    Console.WriteLine($"Upload of article {article.Sku} failed");
+        }
+
+        private bool UploadArticle(Article article)
+            => _rng.Next(2) == 0;
     }
 }
