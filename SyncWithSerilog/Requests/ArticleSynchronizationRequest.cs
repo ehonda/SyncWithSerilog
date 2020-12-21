fa9573ea690caea05f42fbf6402c817f56d793c5
Dennis Renz <dennis@r-renz.de>
2020-12-21 20:32:43 +0100

    Add parsed request

---
+++ SyncWithSerilog/Requests/ArticleSynchronizationRequest.cs
@@ -0,0 +1,16 @@
+﻿using SyncWithSerilog.Filters;
+using System;
+
+namespace SyncWithSerilog.Requests
+{
+    public record ArticleSynchronizationRequest(int Count, double SuccessRate)
+    {
+        public static ArticleSynchronizationRequest FromFilter(
+            ArticleSynchronizationRequestFilter filter)
+            => filter switch
+            {
+                null => throw new ArgumentNullException(nameof(filter)),
+                var (count, successRate) => new(count ?? 0, successRate ?? .5)
+            };
+    }
+}
